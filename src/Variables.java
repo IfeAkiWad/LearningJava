beeb1acda38b647afd37dd6ff83e0dff58e02125
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-04-19 21:31:14 -0500

    practice declaring java variables

--- src/Variables.java
+++ src/Variables.java
@@ -1,12 +1,25 @@
 public class Variables {
-   static void intVariables() {
+   static void strVariables() {
         String practice = "I'm growing my coding muscle by learning ";
         String language = "Java";
 
+        String experience = "My experience so far has been ok. I would rate Java with a(n) ";
+        char rate = 'A';
+
         System.out.println(practice + language);
+        System.out.println(experience + rate);
+    }
+
+    static void intVariables() {
+        int days = 1 + 2;
+        int blogs = 2;
+        String sentence = "I have been learning for " + days + " days, and have written " + blogs + " blogs";
+        
+        System.out.println(sentence);
     }
 
     public static void main(String[] args) throws Exception {
+        strVariables();
         intVariables();
         
     }
