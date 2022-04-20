12889866440012fe69603337c58bfe00e3810385
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-04-19 21:12:08 -0500

    create Variable class

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+   static void intVariables() {
+        String practice = "I'm growing my coding muscle by learning ";
+        String language = "Java";
+
+        System.out.println(practice + language);
+    }
+
+    public static void main(String[] args) throws Exception {
+        intVariables();
+        
+    }
+}
