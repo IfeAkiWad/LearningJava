234fc3cee5ac10b4afed8920d8859df35ebc77ac
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-06 00:02:07 -0500

    learn constructor parameters

--- src/ClassConstructor.java
+++ src/ClassConstructor.java
@@ -1,12 +1,13 @@
 public class ClassConstructor {
     String forConstructor;
 
-    public ClassConstructor() {
+    public ClassConstructor(String x) {
         forConstructor = "This is an attribute initialized in a constructor";
+        forConstructor = x;
     }
 
     public static void main(String[] args) {
-        ClassConstructor myObj = new ClassConstructor(); 
+        ClassConstructor myObj = new ClassConstructor("work"); 
         System.out.println(myObj.forConstructor); 
     }
 }
