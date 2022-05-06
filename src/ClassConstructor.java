b89fc895f5de6e148fdea4c0b62b97ae0c3270f2
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-06 00:23:25 -0500

    learn constructor parameters

--- src/ClassConstructor.java
+++ src/ClassConstructor.java
@@ -1,25 +1,15 @@
 public class ClassConstructor {
     String forConstructor;
-    String humanName;
-    int humanAge;
     
-    public ClassConstructor(String x, String name, int age) {
+    
+    public ClassConstructor(String x) {
         forConstructor = "This is an attribute initialized in a constructor";
         forConstructor = x;
-
-        humanName = name;
-        humanAge = age;
-    }
-
-    public void ClassConstructorTwo(String name, int age) {
-       humanName = name;
-        humanAge = age;
     }
 
     public static void main(String[] args) {
         ClassConstructor myObj = new ClassConstructor("work"); 
         System.out.println(myObj.forConstructor);
         
-        ClassConstructor human = new ClassConstructorTwo("Bill", 32); 
     }
 }
