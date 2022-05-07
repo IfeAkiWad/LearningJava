a4359fe1c0e9761162ac36b3e180de3ecea6b47c
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-06 21:51:13 -0500

    learn Java Encapsulation

--- src/ClassConstructorTwo.java
+++ src/ClassConstructorTwo.java
@@ -11,5 +11,8 @@ public class ClassConstructorTwo {
     public static void main(String[] args) {
         ClassConstructorTwo human = new ClassConstructorTwo("Bill", 32); 
         System.out.println(human.humanName + " " + human.humanAge);
+        Encapsulation lastName = new Encapsulation();
+        lastName.setLastName("Edwards");
+        System.out.println(human.humanName + " " + lastName.getLastName() + " " + human.humanAge);
     }
 }
---
+++ src/Encapsulation.java
@@ -0,0 +1,13 @@
+public class Encapsulation {
+    private String lastName;
+
+    // getter
+    public String getLastName() {
+        return lastName;
+    }
+
+    // setter
+    public void setLastName(String theLastName){
+        this.lastName = theLastName;
+    }
+}
