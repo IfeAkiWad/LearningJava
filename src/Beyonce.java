43e1c0bdec398e288ab97cf177c6fb00ae23f7bf
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-15 22:11:44 -0700

    learn Java abstraction

--- src/Beyonce.java
+++ src/Beyonce.java
@@ -2,6 +2,11 @@ public class Beyonce extends Artist{
     public void song() {
         System.out.println("Sorry");
     }
+
+    public static void main(String[] args) {
+        Beyonce beyonce = new Beyonce();
+        beyonce.song();
+      }
 }
 
 class Main {
