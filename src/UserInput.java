ea3ef1ab146a2e2502acce32de98cfd24c675f01
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-19 22:41:37 -0700

    learn Java user input

--- src/UserInput.java
+++ src/UserInput.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args) {
-        try (Scanner obj = new Scanner(System.in)) {
-            System.out.println("Hello, friend");
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Hello, friend");
 
-            String userInput = obj.nextLine();
-            System.out.println(userInput);
-        }
+        String userInput = obj.nextLine();
+        System.out.println(userInput);
     }
 }
