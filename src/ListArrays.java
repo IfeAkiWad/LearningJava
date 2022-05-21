6f7f083ee15697387d1714aeea0841b3bfc7b888
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-20 19:21:58 -0700

    learn Java ArrayList

---
+++ src/ListArrays.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ListArrays {
+    public static void main(String[] args) {
+        ArrayList<String> planet = new ArrayList<String>();
+
+        planet.add("Mercury");
+        planet.add("Venus");
+        planet.add("Earth");
+
+        System.out.println(planet);
+    }
+}
